a37c3790a43062109e010d5e230ff49b388ca26e
chenzhenxxx <1922816632@qq.com>
2022-07-16 11:32:37 +0800

    c/s test

--- client.cpp
+++ client.cpp
@@ -8,9 +8,55 @@
 #include<stdlib.h>
 #include<fcntl.h>
 #include<iostream>
+#include <nlohmann/json.hpp>
 using namespace std;
 #define PORT 10000
 #define MAXLEN 4096
+using json= nlohmann::json;
+
+
+namespace jjjson
+{
+    class stu
+    {
+    public:
+        string name;
+        int age;
+        int sex;
+    };
+    class pwd
+    {  public:
+        string name;
+        string pwd;
+    };
+    void to_json(json &j, const stu &p)
+    {
+        j = json{{"name", p.name}, {"sex", p.sex}, {"age", p.age}};
+    }
+
+    void from_json(const json &j, stu &p)
+    {
+        j.at("name").get_to(p.name);
+        j.at("sex").get_to(p.sex);
+        j.at("age").get_to(p.age);
+    };
+    void to_json(json &j, const pwd &p)
+    {
+        j = json{{"name", p.name}, {"pwd", p.pwd}};
+    }
+
+    void from_json(const json &j,pwd &p)
+    {
+        j.at("name").get_to(p.name);
+        j.at("pwd").get_to(p.pwd);
+    };
+}
+
+
+
+
+
+
 int main(int argc,char **argv)
 {
     struct sockaddr_in serveraddr;
@@ -25,11 +71,17 @@ int main(int argc,char **argv)
      serveraddr.sin_port=htons(PORT);
 
      connect(cfd,(struct sockaddr *)&serveraddr,sizeof(serveraddr)); //将cfd的主动socket连接到由serveraddr指定的监听socket；
-     while(1)
-     { printf("qsr !\n");
-       char buf[4096];
-       scanf("%s",buf);
-       send(cfd,buf,strlen(buf),0);
+     //while(1)
+     { //printf("qsr !\n");
+       //char buf[4096];
+       //scanf("%s",buf);
+        jjjson::pwd p;
+          p.name="czx";
+          p.pwd="123";
+           json x=p;
+           cout << x << endl;
+           string m=x.dump();
+       send(cfd,m.c_str(),m.size(),0);
        
      }
      close(cfd);
--- server.cpp
+++ server.cpp
@@ -9,9 +9,47 @@
 #include <errno.h>
 #include <sys/epoll.h>
 #include<fcntl.h>
+#include<nlohmann/json.hpp>
 #define PORT 10000
 #define MAXLEN 4096
 using namespace std;
+using json =nlohmann::json;
+namespace jjjson
+{
+    class stu
+    {
+    public:
+        string name;
+        int age;
+        int sex;
+    };
+    class pwd
+    {  public:
+        string name;
+        string pwd;
+    };
+    void to_json(json &j, const stu &p)
+    {
+        j = json{{"name", p.name}, {"sex", p.sex}, {"age", p.age}};
+    }
+
+    void from_json(const json &j, stu &p)
+    {
+        j.at("name").get_to(p.name);
+        j.at("sex").get_to(p.sex);
+        j.at("age").get_to(p.age);
+    };
+    void to_json(json &j, const pwd &p)
+    {
+        j = json{{"name", p.name}, {"pwd", p.pwd}};
+    }
+
+    void from_json(const json &j,pwd &p)
+    {
+        j.at("name").get_to(p.name);
+        j.at("pwd").get_to(p.pwd);
+    };
+}
 int main()
 {
     struct sockaddr_in serveraddr;
@@ -85,7 +123,11 @@ int main()
                     }
                     else
                     {   buf[strlen(buf)]='\0';
-                        write(STDOUT_FILENO,buf,sizeof(buf));
+                        json j=json::parse(buf);
+                        auto m = j.get<jjjson::pwd>();
+                        cout<<m.name<<endl;
+                        cout<<m.pwd<<endl;
+                        cout<<j<<endl;
                     }
                 }
             }
